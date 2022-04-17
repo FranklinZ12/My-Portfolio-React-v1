98d72827d868280aee71af9fac7e0f21c2b813e1
FranklinZ12 <franklinq8@gmail.com>
2022-04-17 17:47:59 -0500

    apartado de skills hecho como componente

---
+++ src/components/about/Skills.jsx
@@ -0,0 +1,35 @@
+import html5 from '../../assets/html-5.png';
+import css from '../../assets/css3.png';
+import react from '../../assets/react.png';
+import js from '../../assets/javascript.png';
+import nj from '../../assets/nodejs.png';
+import git from '../../assets/git.png';
+import github from '../../assets/github.png';
+import scrum from '../../assets/scrum.png';
+import CardSkills from './CardSkills';
+
+
+const Skills = () => {
+  return (
+      <div className='skills container'>
+          <div className='section-title'>
+              <h2>Skills</h2>
+          </div>
+
+          <div className='skills-content'>
+              <div className='grid grid-cols-4 gap-y-9 gap-x-5 sm:grid-cols-2 lg:grid-cols-4'>
+                  <CardSkills img={react} name='REACTJS' />
+                  <CardSkills img={html5} name='HTML' />
+                  <CardSkills img={css} name='CSS' />
+                  <CardSkills img={js} name='JAVASCRIPT' />
+                  <CardSkills img={nj} name='NODEJS' />
+                  <CardSkills img={git} name='GIT' />
+                  <CardSkills img={github} name='GITHUB' />
+                  <CardSkills img={scrum} name='SCRUM' />
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default Skills
--- src/pages/AboutScreen.jsx
+++ src/pages/AboutScreen.jsx
@@ -1,15 +1,8 @@
 import user from 'assets/user.jpg';
+import Skills from 'components/about/Skills';
 import { useMatch } from 'react-router-dom';
 import 'styles/about.css';
-import html5 from '../assets/html-5.png';
-import css from '../assets/css3.png';
-import react from '../assets/react.png';
-import js from '../assets/javascript.png';
-import nj from '../assets/nodejs.png';
-import git from '../assets/git.png';
-import github from '../assets/github.png';
-import scrum from '../assets/scrum.png';
-import CardSkills from 'components/about/CardSkills';
+
 const AboutScreen = () => {
   const match = useMatch('/about');
 
@@ -75,26 +68,9 @@ const AboutScreen = () => {
       </div>
 
       {/* ======= Skills  =======  */}
-      <div className='skills container'>
-        <div className='section-title'>
-          <h2>Skills</h2>
-        </div>
-
-        <div className='skills-content'>
-          <div className='grid grid-cols-4 gap-y-9 gap-x-5 sm:grid-cols-2 lg:grid-cols-4'>
-            <CardSkills img={react} name='REACTJS' />
-            <CardSkills img={html5} name='HTML' />
-            <CardSkills img={css} name='CSS' />
-            <CardSkills img={js} name='JAVASCRIPT' />
-            <CardSkills img={nj} name='NODEJS' />
-            <CardSkills img={git} name='GIT' />
-            <CardSkills img={github} name='GITHUB' />
-            <CardSkills img={scrum} name='SCRUM' />
-          </div>
-        </div>
-      </div>
+      <Skills />
       {/* End Skills  */}
-      
+
     </section>
   )
 }
